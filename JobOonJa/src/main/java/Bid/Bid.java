ceb21c3b325efdc90d53e34a0bb61e07d3d907c1
amir <iamirranjbar@gmail.com>
2019-02-08 01:23:35 +0330

    Bid class has been added.

--- JobOonJa/src/main/java/Bid/Bid.java
+++ JobOonJa/src/main/java/Bid/Bid.java
@@ -1,4 +1,16 @@
 package Bid;
 
+import Project.Project;
+import User.User;
+
 public class Bid {
+    private User user;
+    private Project project;
+    private int price;
+
+    public Bid(User user, Project project, int price){
+        this.user = user;
+        this.project = project;
+        this.price = price;
+    }
 }
