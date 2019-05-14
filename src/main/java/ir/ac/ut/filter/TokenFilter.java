54bca2b8f53f60cab6a8192f95a3efb61b1cb8da
AliEdalat <ali.edalat.77@gmail.com>
2019-05-14 19:51:59 +0430

    Add token filter.

---
+++ src/main/java/ir/ac/ut/filter/TokenFilter.java
@@ -0,0 +1,21 @@
+package ir.ac.ut.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class TokenFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
