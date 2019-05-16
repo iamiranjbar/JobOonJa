75b21b36ea368c5e01b69c3e91b0669974dbd042
AliEdalat <ali.edalat.77@gmail.com>
2019-05-16 15:51:08 +0430

    Fix TokenFilter class.

--- src/main/java/ir/ac/ut/Main.java
+++
@@ -1,9 +0,0 @@
-package ir.ac.ut;
-
-import ir.ac.ut.jwt.Jwt;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Jwt.createJWT("1","ali","login", 12345667));
-    }
-}
--- src/main/java/ir/ac/ut/filter/TokenFilter.java
+++ src/main/java/ir/ac/ut/filter/TokenFilter.java
@@ -1,21 +1,55 @@
 package ir.ac.ut.filter;
 
-import javax.servlet.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureException;
+import ir.ac.ut.jwt.Jwt;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class TokenFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
+public class TokenFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+        final String authHeader = request.getHeader("authorization");
 
-    }
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            if (authHeader == null) {
+                System.out.println("null");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().println("header is null.");
+            }
 
-    @Override
-    public void destroy() {
+            if (!authHeader.startsWith("Bearer ")) {
+                System.out.println("bad header");
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.getWriter().println("header style is not good.");
+            }
 
+            final String token = authHeader.substring(7);
+            System.out.println(token);
+
+            try {
+                final Claims claims = Jwt.decodeJWT(token);
+                request.setAttribute("claims", claims);
+            } catch (final SignatureException e) {
+                System.out.println("bad header");
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.getWriter().println("SignatureException.");
+            }
+
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
     }
 }
